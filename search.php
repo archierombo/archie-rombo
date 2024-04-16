8c2e1ec9b00309cabac8cb1fcdfaec4dc2b9d59f
Archie Rombo <archierombo@gmail.com>
2024-04-16 13:04:59 +0300

    Search Results Page Beautified

--- search.php
+++ search.php
@@ -1,11 +1,12 @@
 <?php get_header();?>
 
 <section class="page-wrap">
-	<div class="container">
+	<div class="container-fluid" style="--bs-gutter-x: 1.5rem;width: 95%;">
 		
 				
 				<!--add content of /includes/section-archive.php to this page-->
 				<h1>Search Results for '<?php echo get_search_query(); ?>'</h1>
+				<p><?php get_search_form(); ?></p>
 
 				<?php get_template_part('includes/section','searchresults');?>
 
@@ -22,23 +23,25 @@
 
 				<!--pagination wordpress style-->
 				<?php
-				global $wp_query;
+				//global $wp_query;
 
-				$big = 999999999; // need an unlikely integer
-				$translated = __( 'Page', 'archie-rombo' ); // Supply translatable string
+				//$big = 999999999; // need an unlikely integer
+				//$translated = __( 'Page', 'archie-rombo' ); // Supply translatable string
 
-				echo paginate_links( array(
-					'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					'format' => '?paged=%#%',
-					'current' => max( 1, get_query_var('paged') ),
-					'total' => $wp_query->max_num_pages,
-			       'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
-				) );
+				//echo paginate_links( array(
+					//'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+					//'format' => '?paged=%#%',
+					//'current' => max( 1, get_query_var('paged') ),
+					//'total' => $wp_query->max_num_pages,
+			      // 'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
+				//) );
 				?>
 			<!--pagination wordpress style-->
 
 
-
+<!-- pagination developer style-->
+				<?php previous_posts_link();?>
+				<?php next_posts_link();?>
 
 
 	</div>
